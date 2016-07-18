7ed0d717ce56989b701a590d8f92580fc726d5a1
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-18 17:22:05 +0200

    Update 02_docker.sh

--- scripts/02_docker.sh
+++ scripts/02_docker.sh
@@ -1,5 +1,15 @@
 #/bin/bash
 
- docker build -t nginx-ecoinformatics docker 
- docker run --name ecoinformatics.org -d -p 80:80 nginx-ecoinformatics
- docker exec -it ecoinformatics.org /bin/bash
+ IMAGE_NAME="nginx-ecoinformatics"
+ IP="192.168.56.110"
+ HOST="ecoinformaticss.org"
+ FOLDER_DOCKER_FILE="docker"
+ 
+ sudo -- sh -c "echo '$IP $HOST' >> /etc/hosts" 
+ 
+ CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ cd $CURRENT_PATH/$FOLDER_DOCKER_FILE
+   
+ docker build -t $IMAGE_NAME .
+ docker run -d --net mynet123 --name $HOST  --ip $IP -d -p 80:80 $IMAGE_NAME
+ 
