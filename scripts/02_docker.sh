244a1d8e0c72bf0b72ceea0902b46cd0ccde4367
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-18 17:31:57 +0200

    Update 02_docker.sh

--- scripts/02_docker.sh
+++ scripts/02_docker.sh
@@ -5,7 +5,8 @@
  HOST="ecoinformaticss.org"
  FOLDER_DOCKER_FILE="docker"
  
- sudo -- sh -c "echo '$IP $HOST' >> /etc/hosts" 
+ LINE="$IP $HOST"
+ sudo -- sh -c "echo '$LINE' >> /etc/hosts" 
  
  CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
  cd $CURRENT_PATH/$FOLDER_DOCKER_FILE
@@ -13,3 +14,5 @@
  docker build -t $IMAGE_NAME .
  docker run -d --net mynet123 --name $HOST  --ip $IP -d -p 80:80 $IMAGE_NAME
  
+ # On Stop
+ #sudo sed -i "s/$LINE/#$LINE/g" /etc/hosts
