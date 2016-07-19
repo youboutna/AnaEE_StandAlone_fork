9526be7c9b0ba9505e76345254116c3ec941bf84
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-19 09:36:03 +0200

    Update 02_docker.sh

--- scripts/02_docker.sh
+++ scripts/02_docker.sh
@@ -1,18 +1,40 @@
 #/bin/bash
 
+ DEFAULT_IP="192.168.56.110"
  IMAGE_NAME="nginx-ecoinformatics"
- IP="192.168.56.110"
  HOST="ecoinformaticss.org"
  FOLDER_DOCKER_FILE="docker"
  
- LINE="$IP $HOST"
- sudo -- sh -c "echo '$LINE' >> /etc/hosts" 
- 
  CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
- cd $CURRENT_PATH/$FOLDER_DOCKER_FILE
-   
- docker build -t $IMAGE_NAME .
- docker run -d --net mynet123 --name $HOST  --ip $IP -d -p 80:80 $IMAGE_NAME
+ cd $CURRENT_PATH
  
- # On Stop
- #sudo sed -i "s/$LINE/#$LINE/g" /etc/hosts
+ if [ "$1" = "start" ] ; then 
+
+    LINE="$IP $HOST"
+    sudo -- sh -c "echo '$LINE' >> /etc/hosts" 
+    
+    CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    cd $CURRENT_PATH/$FOLDER_DOCKER_FILE
+      
+    docker build -t $IMAGE_NAME .
+    docker run -d --net mynet123 --name $HOST  --ip $DEFAULT_IP -d -p 80:80 $IMAGE_NAME
+    
+ fi
+ 
+ if [ "$1" = "stop" ] ; then 
+
+    removeContainerBasedOnImage() {
+       IMAGE=$1
+       echo
+       echo -e " Remove container based on images  $IMAGE "
+       docker ps -a | awk '{ print $1,$2 }' | grep $IMAGE | awk '{print $1 }' | xargs -I {} docker rm -f {}
+       echo -e " All containers removed !! "
+       echo
+    } 
+    
+    removeAllContainerBasedOnImage $IMAGE_NAME
+    
+    sudo sed --in-place '/$LINE/d' /etc/hosts
+   
+ fi
+ 
