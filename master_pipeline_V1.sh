4107a66758bb7c4a79720fd558de969b5f35930a
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-19 17:16:39 +0200

    Create master_pipeline_V1.sh

---
+++ master_pipeline_V1.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# $1 IP_HOST  
+# $2 NAME_SPACE  
+# $3 LOCAL_PORT 
+# $4 REMOTE_PORT
+# $5 DATAB_BASE { [postgresql] - mysql } 
+
+    if [ $# -eq 4 -o $# -eq 5 ] ; then
+
+    chmod -R +x scripts/*
+    
+    ./scripts/02_docker.sh stop
+    
+    ./scripts/03_nano_start_stop.sh stop
+        
+    ./scripts/utils/check_commands.sh
+        
+    ./scripts/00_install_libs.sh $5
+    
+    ./scripts/01_build_config.sh  $1 $2 $3 $4
+
+    ./scripts/02_docker.sh start
+    
+    ./scripts/03_nano_start_stop.sh start rw
+   
+    
+    for entry in `ls -d data/yedGen/*/`; do
+    
+        if [[ "$entry" != */connexion/ ]] ; then
+        
+          rm data/yedGen/*.*
+          rm data/ontop/*.*
+          rm data/corese/*.* && rm data/corese/*
+                    
+          cp data/yedGen/connexion/connexion.graphml data/yedGen/
+          cp -R $entry/*.*  data/yedGen
+                
+          ./scripts/04_gen_mapping.sh
+     
+          ./scripts/05_ontop_gen_triples.sh
+    
+          ./scripts/06_corese_infer.sh
+    
+          ./scripts/07_load_data.sh
+        
+          sleep 30
+          
+        fi
+    
+    done
+    
+    ./scripts/02_docker.sh stop
+    
+    ./scripts/08_portail_query.sh ola_portal_synthesis.ttl
+    
+    else
+
+        echo
+        echo " Invalid arguments :  please pass Four or Five arguments  "
+        echo " arg_1             :  IP_HOST ( or Hostname )             "
+        echo " arg_2             :  NAME_SPACE                          "
+        echo " arg_3             :  LOCAL_PORT                          "
+        echo " arg_4             :  REMOTE_PORT                         "        
+        echo " arg_5             :  DATA_BASE { [postgresql] - mysql }  "
+        echo
+    fi
