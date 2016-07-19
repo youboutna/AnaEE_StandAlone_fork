bed048a9ef0413518014ed6a1424b7bfab333f84
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-19 10:11:08 +0200

    Update 02_docker.sh

--- scripts/02_docker.sh
+++ scripts/02_docker.sh
@@ -7,32 +7,40 @@
  FOLDER_DOCKER_FILE="docker"
  
  CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
- cd $CURRENT_PATH
- 
+ cd $CURRENT_PATH/$FOLDER_DOCKER_FILE
+
+ removeContainerBasedOnImage() {
+   IMAGE=$1
+   echo
+   echo -e " Remove container based on image  $IMAGE "
+   docker ps -a | awk '{ print $1,$2 }' | grep $IMAGE | awk '{print $1 }' | xargs -I {} docker rm -f {}
+   echo -e " Containers removed !! "
+   echo
+ } 
+    
  if [ "$1" = "start" ] ; then 
 
     LINE="$IP $HOST"
     sudo -- sh -c "echo '$LINE' >> /etc/hosts" 
-    
-    CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    cd $CURRENT_PATH/$FOLDER_DOCKER_FILE
       
-    docker build --rm -t $IMAGE_NAME .
-    docker run -d --net mynet123 --name $HOST --ip $DEFAULT_IP -d -p $DEFAULT_PORT:$DEFAULT_PORT $IMAGE_NAME
+    if docker history -q $IMAGE_NAME >/dev/null 2>&1 ; then
+	
+	       removeContainerBasedOnImage $DOCKER_BLZ_IMAGE
+                    
+        echo " Remove Image  $IMAGE_NAME ... "
+        docker rmi -f $IMAGE_NAME
+        echo " Images removed !! "
+        echo
+    else 
+        fuser -k $DEFAULT_PORT/tcp
+        docker build -t $IMAGE_NAME .
+        docker run -d --net mynet123 --name $HOST --ip $DEFAULT_IP -d -p $DEFAULT_PORT:$DEFAULT_PORT $IMAGE_NAME
+    fi
     
  fi
  
  if [ "$1" = "stop" ] ; then 
 
-    removeContainerBasedOnImage() {
-       IMAGE=$1
-       echo
-       echo -e " Remove container based on images  $IMAGE "
-       docker ps -a | awk '{ print $1,$2 }' | grep $IMAGE | awk '{print $1 }' | xargs -I {} docker rm -f {}
-       echo -e " All containers removed !! "
-       echo
-    } 
-    
     removeContainerBasedOnImage $IMAGE_NAME
     
     sudo sed --in-place '/$LINE/d' /etc/hosts
