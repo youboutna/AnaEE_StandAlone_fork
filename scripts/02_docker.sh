18af25d2d79de549cc8cfafb5353e39818821e64
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-19 10:35:56 +0200

    Update 02_docker.sh

--- scripts/02_docker.sh
+++ scripts/02_docker.sh
@@ -6,8 +6,9 @@
  DEFAULT_PORT="80"
  IMAGE_NAME="nginx-ecoinformatics"
  HOST="ecoinformaticss.org"
+ REDIRECTION="localhost"
  FOLDER_DOCKER_FILE="docker"
- 
+
  CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
  cd $CURRENT_PATH/$FOLDER_DOCKER_FILE
 
@@ -34,22 +35,21 @@
     
     if docker history -q $IMAGE_NAME >/dev/null 2>&1 ; then
 	
-        removeContainerBasedOnImage $DOCKER_BLZ_IMAGE
+        removeContainerBasedOnImage $IMAGE_NAME
                     
         #echo " Remove Image  $IMAGE_NAME ... "
         #docker rmi -f $IMAGE_NAME
         #echo " Images removed !! "
         #echo
-        
-    else 
-    
-        fuser -k $DEFAULT_PORT/tcp
-        LINE="$IP $HOST"
-        sudo -- sh -c "echo '$LINE' >> /etc/hosts" 
-      
-        docker build -t $IMAGE_NAME .
-        docker run -d --net mynet123 --name $HOST --ip $DEFAULT_IP -d -p $DEFAULT_PORT:$DEFAULT_PORT $IMAGE_NAME
     fi
+    
+    fuser -k $DEFAULT_PORT/tcp
+    LINE="$DEFAULT_IP $REDIRECTION"
+    sudo -- sh -c "echo '$LINE' >> /etc/hosts" 
+    
+    docker build -t $IMAGE_NAME .
+    docker run -d --net $SUBNET --name $HOST --ip $DEFAULT_IP -d -p $DEFAULT_PORT:$DEFAULT_PORT $IMAGE_NAME
+    
     
  fi
  
