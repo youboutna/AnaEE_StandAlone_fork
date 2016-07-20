4fda2918cd9cb18fb9e97df280152a760fa79ef6
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-20 22:04:17 +0200

    Update master_pipeline_demo.sh

--- master_pipeline_demo.sh
+++ master_pipeline_demo.sh
@@ -9,10 +9,10 @@
 if [ $# -eq 4 -o $# -eq 5 ] ; then
 
   chmod -R +x scripts/*
+  
+  ./scripts/utils/check_commands.sh java curl psql-mysql mvn
    
   ./scripts/03_nano_start_stop.sh stop
-    
-  ./scripts/utils/check_commands.sh java curl psql-mysql mvn
    
   ./scripts/00_install_libs.sh $5
   
