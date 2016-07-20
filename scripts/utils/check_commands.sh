87369c47fa558b541324891aa8828ed630aeb732
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-20 14:19:45 +0200

    Update check_commands.sh

--- scripts/utils/check_commands.sh
+++ scripts/utils/check_commands.sh
@@ -15,27 +15,34 @@
   script_name="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
   parent_script=`ps -ocommand= -p $PPID | awk -F/ '{print $NF}' | awk '{print $1}'`
   
+  checkCommand() {
+  
+    com=$1
+    
+    if which $com >/dev/null ; then
+        
+        echo " OK .... $com "
+        sleep 0.4
+       
+    else
+     
+        echo
+        echo -e "\e[91m $com  : Command not found. Aborting \e[39m" ; 
+        echo
+      
+        if [ $parent_script = "bash" ] ; then
+            exit 2
+        else
+            kill -9 `ps --pid $$ -oppid=`;
+            exit 2
+        fi
+    fi
+  }
+  
+  
   for com in "$@" ; do
     
-      if which $com >/dev/null ; then
-        
-          echo " OK .... $com "
-          sleep 0.4
-        
-      else
-      
-          echo
-          echo -e "\e[91m $com  : Command not found. Aborting \e[39m" ; 
-          echo
-        
-          if [ $parent_script = "bash" ] ; then
-              exit 2
-          else
-              kill -9 `ps --pid $$ -oppid=`;
-              exit 2
-          fi
-        
-      fi
+     checkCommand $com
   
   done
 
