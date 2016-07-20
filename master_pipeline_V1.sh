706f5429b359173498c08539f0acbb11f9f5e77a
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-20 10:09:40 +0200

    Update master_pipeline_V1.sh

--- master_pipeline_V1.sh
+++ master_pipeline_V1.sh
@@ -7,52 +7,74 @@
 # $5 DATAB_BASE { [postgresql] - mysql } 
 
     if [ $# -eq 4 -o $# -eq 5 ] ; then
+    
+         YED_GEN_FOLDER="data/yedGen"
+         EXTENSION_FILE="graphml"
+         CONNEXION_FILE_PATTERN="$YED_GEN_FOLDER/connexion/connexion"
+         CONNEXION_FILE="$CONNEXION_FILE_PATTERN.$EXTENSION_FILE"
+         ONTOP_FOLDER="data/ontop"
+         CORESE_FOLDER="data/corese"
+         
+         ClearFolders() {
+         
+            # Clear folders on each iteration
+              
+            rm $YED_GEN_FOLDER/*.*
+            rm $ONTOP_FOLDER/*.*
+            rm $CORESE_FOLDER/*.* && rm $CORESE_FOLDER/*
+         }  
 
-    chmod -R +x scripts/*
-    
-    ./scripts/02_docker.sh stop
-    
-    ./scripts/03_nano_start_stop.sh stop
+         chmod -R +x scripts/*
         
-    ./scripts/utils/check_commands.sh
+        ./scripts/02_docker.sh stop
         
-    ./scripts/00_install_libs.sh $5
-    
-    ./scripts/01_build_config.sh  $1 $2 $3 $4
-
-    ./scripts/02_docker.sh start
-    
-    ./scripts/03_nano_start_stop.sh start rw
-   
-    
-    for entry in `ls -d data/yedGen/*/`; do
-    
-        if [[ "$entry" != */connexion/ ]] ; then
+        ./scripts/03_nano_start_stop.sh stop
+            
+        ./scripts/utils/check_commands.sh
+            
+        ./scripts/00_install_libs.sh $5
         
-          rm data/yedGen/*.*
-          rm data/ontop/*.*
-          rm data/corese/*.* && rm data/corese/*
-                    
-          cp data/yedGen/connexion/connexion.graphml data/yedGen/
-          cp -R $entry/*.*  data/yedGen
+        ./scripts/01_build_config.sh  $1 $2 $3 $4
+    
+        ./scripts/02_docker.sh start
+        
+        ./scripts/03_nano_start_stop.sh start rw
+        
+        
+        for entry in `find $YED_GEN_FOLDER/* -type d -not -name '*connexion*'`; do
+            
+              ClearFolders
+              
+              # Copy $CONNEXION_FILE  file to $YED_GEN_FOLDER Folder
+             
+              cp $CONNEXION_FILE $YED_GEN_FOLDER
+              
+              # Copy all files in $entry to $YED_GEN_FOLDER Folder
+              
+              cp -R $entry/*.*  $YED_GEN_FOLDER
+              
+              # Check if $YED_GEN_FOLDER Folder contains more than 2 files ( connexion.graphml included )
+              
+              if [ `ls -l $YED_GEN_FOLDER | egrep -c '^-'` -gt 1 ] ; then 
                 
-          ./scripts/04_gen_mapping.sh
-     
-          ./scripts/05_ontop_gen_triples.sh
-    
-          ./scripts/06_corese_infer.sh
-    
-          ./scripts/07_load_data.sh
+                 ./scripts/04_gen_mapping.sh
+         
+                 ./scripts/05_ontop_gen_triples.sh
         
-          sleep 30
-          
-        fi
+                 ./scripts/06_corese_infer.sh
+        
+                 ./scripts/07_load_data.sh
+            
+                 sleep 0.5
+              
+              fi
+        done
     
-    done
+        ./scripts/02_docker.sh stop
+        
+        ClearFolders
     
-    ./scripts/02_docker.sh stop
-    
-    ./scripts/08_portail_query.sh ola_portal_synthesis.ttl
+        ./scripts/08_portail_query.sh data/portail/ola_portal_synthesis.ttl
     
     else
 
