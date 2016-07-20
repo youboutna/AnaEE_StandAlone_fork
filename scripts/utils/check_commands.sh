b010cd1ce94c5f13033ae55754b2e6fb3e3d1c6a
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-20 13:46:23 +0200

    Update check_commands.sh

--- scripts/utils/check_commands.sh
+++ scripts/utils/check_commands.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+  # java - curl - [postgres] - maven
+  tput setaf 2
+  echo 
+  echo -e " ################################# "
+  echo -e " ######### Check Commands ######## "
+  echo -e " --------------------------------- "
+  echo -e " \e[90m$0        \e[32m            "
+  echo
+  sleep 1
+  tput setaf 7
+  
+  script_name="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+  parent_script=`ps -ocommand= -p $PPID | awk -F/ '{print $NF}' | awk '{print $1}'`
+  
+  #!/bin/bash
+
   tput setaf 2
   echo 
   echo -e " ################################# "
@@ -10,46 +26,29 @@
   sleep 1
   tput setaf 7
     
-  command -v java >/dev/null 2>&1 || { 
-     echo
-     echo " Require JAVA but it's not installed.  Aborting. " ; 
-     echo
-     kill -9 `ps --pid $$ -oppid=` ; 
-     exit
-  }
-  echo " java       installed.. "
-  sleep 0.4
-   
-  command -v curl >/dev/null 2>&1 || { 
-     echo
-     echo " Require CURL but it's not installed.  Aborting. " ; 
-     echo
-     kill -9 `ps --pid $$ -oppid=` ; 
-     exit
-  }
-  echo " curl       installed.. "
-  sleep 0.4
+  script_name="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+  parent_script=`ps -ocommand= -p $PPID | awk -F/ '{print $NF}' | awk '{print $1}'`
   
-  command -v psql >/dev/null 2>&1 || { 
-     echo
-     echo " Require POSTGRES but it's not installed.  Aborting. " ; 
-     echo
-     kill -9 `ps --pid $$ -oppid=` ; 
-     exit
-  }
-  echo " postgres   installed.. "
-  sleep 0.4
+  for com in "$@" ; do
+    
+     command -v $com >/dev/null 2>&1 || {
+       
+        echo
+        echo " Require $com but it's not installed.  Aborting. " ; 
+        echo
+        
+        if [ $parent_script = "bash" ] ; then
+            exit 2
+        else
+            kill -9 `ps --pid $$ -oppid=`; 
+        fi    
+     }
   
-  command -v mvn >/dev/null 2>&1 || { 
-     echo
-     echo " Require MAVEN but it's not installed.  Aborting. " ; 
-     echo
-     kill -9 `ps --pid $$ -oppid=` ; 
-     exit
-  }
-  echo " maven      installed.. "
-  sleep 0.4
- 
+     echo " OK....   $com "
+     sleep 0.4
+  
+  done
+
   echo
   
   
