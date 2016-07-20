09b3676beb6fb374dbb3a2d4dcd6637a549b3f54
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-20 14:10:27 +0200

    Update check_commands.sh

--- scripts/utils/check_commands.sh
+++ scripts/utils/check_commands.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-  # java - curl - [postgres] - maven
+  # java - curl - [psql - mysql] - maven
+  
   tput setaf 2
   echo 
   echo -e " ################################# "
@@ -11,44 +12,32 @@
   sleep 1
   tput setaf 7
   
-  script_name="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
-  parent_script=`ps -ocommand= -p $PPID | awk -F/ '{print $NF}' | awk '{print $1}'`
-  
-  #!/bin/bash
-
-  tput setaf 2
-  echo 
-  echo -e " ################################# "
-  echo -e " ######### Check Commands ######## "
-  echo -e " --------------------------------- "
-  echo -e " \e[90m$0        \e[32m            "
-  echo
-  sleep 1
-  tput setaf 7
-    
   script_name="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
   parent_script=`ps -ocommand= -p $PPID | awk -F/ '{print $NF}' | awk '{print $1}'`
   
   for com in "$@" ; do
     
-     command -v $com >/dev/null 2>&1 || {
-       
-        echo
-        echo " Require $com but it's not installed.  Aborting. " ; 
-        echo
+      if which $com >/dev/null ; then
         
-        if [ $parent_script = "bash" ] ; then
-            exit 2
-        else
-            kill -9 `ps --pid $$ -oppid=`; 
-        fi    
-     }
-  
-     echo " OK....   $com "
-     sleep 0.4
+          echo " OK .... $com "
+          sleep 0.4
+        
+      else
+      
+          echo
+          echo -e "\e[91m $com  : Command not found. Aborting \e[39m" ; 
+          echo
+        
+          if [ $parent_script = "bash" ] ; then
+              exit 2
+          else
+              kill -9 `ps --pid $$ -oppid=`;
+              exit 2
+          fi
+        
+      fi
   
   done
 
   echo
-  
   
