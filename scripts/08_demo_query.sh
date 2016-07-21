97743d3bbb0ee001f52ce7e057a8c332db7c02a0
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-21 11:46:40 +0200

    Update 08_demo_query.sh

--- scripts/08_demo_query.sh
+++ scripts/08_demo_query.sh
@@ -1,83 +1,128 @@
 #!/bin/bash
 
-  EXIT() {
-    parent_script=`ps -ocommand= -p $PPID | awk -F/ '{print $NF}' | awk '{print $1}'`
-    if [ $parent_script = "bash" ] ; then
-        exit 2
-    else
-        kill -9 `ps --pid $$ -oppid=`;
-        exit 2
-    fi
-  }
-  
   CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  NANO_END_POINT_FILE="$CURRENT_PATH/conf/nanoEndpoint"
+  cd $CURRENT_PATH
+  
+  if [ $# -eq 1 ] ; then
+  
+       NANO_END_POINT_FILE="$CURRENT_PATH/conf/nanoEndpoint"
+       LINE=$(head -1 $NANO_END_POINT_FILE)
     
-  if [ -f $NANO_END_POINT_FILE ] ; then
-    
-    FIRST_END_POINT=$(head -n 1 $NANO_END_POINT_FILE )
-
-    IFS=’:’ read -ra INFO_NANO <<< "$FIRST_END_POINT" 
-    NANO_END_POINT_IP=${INFO_NANO[1]}
-    NANO_END_POINT_PORT=${INFO_NANO[2]}
-    NANO_END_POINT_NAMESPACE=${INFO_NANO[3]}
-
-    ENDPOINT="http://$NANO_END_POINT_IP:$NANO_END_POINT_PORT/bigdata/namespace/$NANO_END_POINT_NAMESPACE/sparql"
-    
-    RES=`curl -s -I $ENDPOINT | grep HTTP/1.1 | awk {'print $2'}`
-    COUNT=0
-    echo
-    
-    while [ -z $RES ] || [ $RES -ne 200 ] ;do
-       sleep 1
-       RES=`curl -s -I $ENDPOINT | grep HTTP/1.1 | awk {'print $2'}`
-       let "COUNT++" 
+       IFS=$' \t\n' read -ra INFO_NANO <<< "$LINE" 
+       IP=${INFO_NANO[0]}
+       NAMESPACE=${INFO_NANO[1]}
+       PORT=${INFO_NANO[2]}       
+   
+  elif [ $# -eq 4 ] ; then
+       IP=$1
+       PORT=$2
+       NAMESPACE=$3
+       OUT=$4
+       
+  else 
+       echo
+       echo -e "\e[91m 1 or 4 arguments ( OUT_NAME_FILE / IP PORT NAMESAPCE OUT_NAME_FILE ) ! \e[39m "
+       echo
+       exit 3
+  fi
+	
+  PREFIX_ENDPOINT="http://$IP:$PORT"
+  SUFFIX_ENDPOINT="blazegraph/namespace/$NAMESPACE/sparql"
+  
+  ENDPOINT=$PREFIX_ENDPOINT/$SUFFIX_ENDPOINT
+  
+  # Test connexion to namespace 
+  check="cat < /dev/null > /dev/tcp/http://$IP/$PORT"
+      
+  echo ; echo -e " Try connection : $ENDPOINT "
+        
+  TRYING=50
+  COUNT=0
+        
+  timeout 1 bash -c "cat < /dev/null > /dev/tcp/$IP/$PORT" 2> /dev/null
+        
+  OK=$?
+        
+  while [ $OK -ne 0 -a $COUNT -lt $TRYING  ] ; do
+        
+    timeout 1 bash -c "cat < /dev/null > /dev/tcp/$IP/$PORT" 2> /dev/null
            
-       if  [ -z $RES ] || [ $RES != 200 ] ; then 
-          if [ `expr $COUNT % 3` -eq 0 ] ; then
-              echo -e " \e[90m -> attempt to join cluster on namespace $NAMESPACE .. \e[39m"
-          fi
-          if [ $COUNT -eq 20 ] ; then
-              echo -e " \e[31m ENDPOINT $ENDPOINT Not reachable !! \e[39m"
-              echo
-              EXIT
-          fi
-       fi
+    OK=$?
            
-    done
-    
-    tput setaf 2
-    echo -e "-------------------------------------------------------- "
-    echo -e "## Query Demo usning Curl ##                             "
-    echo -e "-----------------------------                            "
-    echo -e "\e[90m$0       \e[32m                                    "
-    echo
-    echo -e " ## EndPoint : $ENDPOINT                                 "
-    echo
-    echo -e "-------------------------------------------------------- "
-    echo 
+    if [ $COUNT == 0 ] ; then echo ; fi 
+           
+    if [ $OK == 1 ] ; then 
+             
+      echo " .. "
+      sleep 0.4 
+             
+    elif [ $OK != 0 ] ; then 
+          
+      echo " attempt ( $COUNT ) : Try again.. "
+      sleep 0.8
+             
+    fi
+           
+    let "COUNT++"
+           
+    if [ $COUNT == $TRYING ] ; then
+          
+      echo
+      echo -e "\e[31m ENDPOINT $ENDPOINT Not reachable !! \e[39m"
+      echo
+      exit 3
+             
+    fi
+           
+  done
+        
+  RES=`curl -s -I $ENDPOINT | grep HTTP/1.1 | awk {'print $2'}`
+        
+  COUNT=0
+        
+  while  [ -z $RES ] || [ $RES -ne 200 ] ;do
+        
     sleep 1
-    tput setaf 7
-  	
-    curl -X POST $ENDPOINT --data-urlencode \
-    'query=PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    RES=`curl -s -I $ENDPOINT | grep HTTP/1.1 | awk {'print $2'}`
+    let "COUNT++" 
+       echo "RES ---> $RES"    
+    if  [ -z $RES ] || [ $RES -ne 200 ]  ; then 
+        if [ `expr $COUNT % 3` -eq 0 ] ; then
+              echo -e " attempt to contact $ENDPOINT .. "
+        fi
+    fi
+           
+  done
+       
+  echo " Yeah Connected !! "
+  
+	tput setaf 2
+  echo 
+  echo -e " ######################################################## "
+  echo -e " ######## Info Synthesis ################################ "
+  echo -e " -------------------------------------------------------- "
+  echo -e " \e[90m$0       \e[32m                                    "
+  echo
+  echo -e " # ENDPOINT  : $PREFIX_ENDPOINT                           "
+  echo -e " # NAMESAPCE : $NAMESPACE                                 "
+  echo -e " # OUT       : $OUT                                       "
+  echo
+  echo -e " ######################################################## "
+  echo 
+  sleep 1
+  tput setaf 7
+        
+  curl -X POST $ENDPOINT/namespace/$NAMESPACE/sparql --data-urlencode \
+  'query=
+           PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
            PREFIX oboe-core: <http://ecoinformatics.org/oboe/oboe.1.0/oboe-core.owl#>
            SELECT ?uri ?unite {
      	        ?uri a oboe-core:Measurement .
    	          ?uri oboe-core:hasValue ?unite
            }
            LIMIT 3
-    ' \
-    -H 'Accept:application/json'
+     ' \
+    -H 'Accept:text/rdf+n3'
    
     echo ; echo 
-    echo -e " \e[47m\e[30m Powered by EcoInfo-Orleans & Ontop Corese BlazeGraph © 2016 \e[49m\e[39m "
-    echo
-   
-  else 
-        
-    echo -e "\e[91m Oupss, config missed !!\e[39m "
- 
-  fi
-    
-    
+
