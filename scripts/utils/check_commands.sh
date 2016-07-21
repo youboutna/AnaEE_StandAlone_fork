e15f4f6a974cbfb462eb09e82b70371ce5fc1bcf
Yahiaoui Rachid <rac021@hotmail.com>
2016-07-21 13:25:51 +0200

    Update check_commands.sh

--- scripts/utils/check_commands.sh
+++ scripts/utils/check_commands.sh
@@ -18,8 +18,10 @@
   EXIT() {
     parent_script=`ps -ocommand= -p $PPID | awk -F/ '{print $NF}' | awk '{print $1}'`
     if [ $parent_script = "bash" ] ; then
+        echo; echo -e " \e[90m exited by : $0 \e[39m " ; echo
         exit 2
     else
+        echo ; echo -e " \e[90m exited by : $0 \e[39m " ; echo
         kill -9 `ps --pid $$ -oppid=`;
         exit 2
     fi
